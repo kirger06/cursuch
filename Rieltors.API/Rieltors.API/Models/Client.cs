232c5c75b330516509222808e8cfdcd17a32a811
kirger06 <kirger06@gmail.com>
2026-04-27 14:19:41 +0300

    add models and create controller

---
+++ Rieltors.API/Rieltors.API/Models/Client.cs
@@ -0,0 +1,9 @@
+﻿namespace Rieltors.API.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Number { get; set;}
+    }
+}
---
+++ Rieltors.API/Rieltors.API/Models/Rieltor.cs
@@ -0,0 +1,12 @@
+﻿namespace Rieltors.API.Models
+{
+    public class Rieltor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Experience { get; set; }
+        public decimal Price { get; set; }
+
+        public int Number { get; set; }
+    }
+}
